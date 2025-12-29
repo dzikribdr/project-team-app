98dee2ee8d1e77417908e8a274ba215fb68fc1c9
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2025-12-29 21:56:43 +0700

    menambahkan class

--- lib/screens/admin/admin_main_screen.dart
+++ lib/screens/admin/admin_main_screen.dart
@@ -5,3 +5,11 @@ import 'package:project_jasun/screens/admin/admin_orders_screen.dart';
 import 'package:project_jasun/screens/admin/admin_users_screen.dart';
 import 'package:project_jasun/screens/auth/login_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+
+class AdminMainScreen extends StatefulWidget {
+  const AdminMainScreen({super.key});
+
+  @override
+  State<AdminMainScreen> createState() => _AdminMainScreenState();
+}
