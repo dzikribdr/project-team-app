1e5e08c814d60c7776200d67d369d52dc9f7f78a
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-01-05 12:20:42 +0700

    membuat Handler untuk background message

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,3 +1,10 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/services.dart'; // Penting untuk fitur Copy
+
+// Handler untuk background message (harus di luar class/top-level)
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // Jika perlu melakukan sesuatu saat background, misal simpan ke local storage
+  print("Handling a background message: ${message.messageId}");
+}
