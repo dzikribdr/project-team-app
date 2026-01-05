42208410d93967cfa8c0ac268cfe4baa31230ae4
dzikriabdr <dzikriabdr@gmail.com>
2026-01-05 22:44:21 +0700

    Pembuatan fungsi untuk mengubah json dari supabase menjadi object product

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -16,4 +16,18 @@ class Product {
     required this.imageUrl,
     required this.categoryId,
   });
+
+  // Fungsi untuk mengubah JSON dari Supabase menjadi Object Product
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] as int,
+      name: json['name'] ?? 'Tanpa Nama',
+      description: json['description'] ?? '',
+      // Supabase kadang mengembalikan angka sebagai int/double, kita paksa jadi double
+      price: (json['price'] as num).toDouble(), 
+      stock: json['stock'] as int,
+      imageUrl: json['image_url'] ?? 'https://via.placeholder.com/150', // Placeholder jika gambar kosong
+      categoryId: json['category_id'] as int,
+    );
+  }
 }
