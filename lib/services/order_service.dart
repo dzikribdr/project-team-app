dd820ac228b1cca9b571ced8f91b4b7e16b7a3a9
dzikriabdr <dzikriabdr@gmail.com>
2026-01-05 23:26:19 +0700

    add basic get orders from supabase

--- lib/services/order_service.dart
+++ lib/services/order_service.dart
@@ -2,4 +2,12 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class OrderService {
   final SupabaseClient _supabase = Supabase.instance.client;
+
+  Future<List<dynamic>> getMyOrders() async {
+    final response = await _supabase
+        .from('orders')
+        .select();
+
+    return response;
+  }
 }
