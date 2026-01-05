cb4cf9c540ea7a46000595f755c4e7b2bca83b51
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-01-05 12:15:24 +0700

    membuat auth login

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  // Login
+  Future<AuthResponse> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await _supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
