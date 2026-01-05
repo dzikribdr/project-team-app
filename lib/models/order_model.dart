88b05ded05e0b3d0629069f37a6be69ad2dca7da
dzikriabdr <dzikriabdr@gmail.com>
2026-01-05 22:46:29 +0700

    Membuat order dalam class order

--- lib/models/order_model.dart
+++ lib/models/order_model.dart
@@ -1,3 +1,13 @@
 class Order {
-  
+  final int id;
+  final double total;
+  final String status;
+  final DateTime createdAt;
+
+  Order({
+    required this.id,
+    required this.total,
+    required this.status,
+    required this.createdAt,
+  });
 }
