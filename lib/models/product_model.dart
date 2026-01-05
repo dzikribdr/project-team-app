6c30fe5196d46f23e713506cd3e9363cb6c34c69
dzikriabdr <dzikriabdr@gmail.com>
2026-01-05 22:43:16 +0700

    Pembuatan product dalam class product

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,3 +1,19 @@
 class Product {
-  
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+  final int stock;
+  final String imageUrl;
+  final int categoryId;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.stock,
+    required this.imageUrl,
+    required this.categoryId,
+  });
 }
