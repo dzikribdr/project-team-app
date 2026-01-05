7092318a1efc72b1761a615e3a01e4897374691f
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-01-05 11:32:53 +0700

    menambahkan class untuk register

--- lib/auth/register_screen.dart
+++ lib/auth/register_screen.dart
@@ -2,3 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:project_jasun/core/constants.dart';
 import 'package:project_jasun/services/auth_service.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
