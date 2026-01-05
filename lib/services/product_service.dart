062f7418c8ed685ed8555dba700229a1d23703d6
dzikriabdr <dzikriabdr@gmail.com>
2026-01-05 23:17:21 +0700

    handle error when fetching products

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -5,11 +5,15 @@ class ProductService {
   final SupabaseClient _supabase = Supabase.instance.client;
 
   Future<List<Product>> getProducts() async {
-    final response = await _supabase
-        .from('products')
-        .select();
+    try {
+      final response = await _supabase
+          .from('products')
+          .select();
 
-    final data = response as List<dynamic>;
-    return data.map((json) => Product.fromJson(json)).toList();
+      final data = response as List<dynamic>;
+      return data.map((json) => Product.fromJson(json)).toList();
+    } catch (e) {
+      throw Exception('Gagal mengambil produk: $e');
+    }
   }
 }
