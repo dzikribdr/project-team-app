4e1aaa9c9dd0d8dfd228c54ec3b09adff1f9a729
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-01-05 12:22:34 +0700

    membuat set untuk notif

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -26,3 +26,19 @@ Future<void> initialize() async {
     } else {
       print('Izin notifikasi ditolak/belum diberikan.');
     }
+
+     // 2. Set Background Handler
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    // 3. Setup Notifikasi Lokal (agar muncul saat aplikasi dibuka/foreground di Android)
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    
+    // Setup untuk iOS (opsional jika nanti perlu)
+    final DarwinInitializationSettings initializationSettingsDarwin =
+        DarwinInitializationSettings();
+
+    final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsDarwin,
+    );
