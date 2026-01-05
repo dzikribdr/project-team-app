a3decf4c16fec8a22a0440c1454941031c84789e
dzikriabdr <dzikriabdr@gmail.com>
2026-01-05 23:14:00 +0700

    add basic get products from supabase

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -2,4 +2,14 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProductService {
   final SupabaseClient _supabase = Supabase.instance.client;
+
+  Future<List<dynamic>> getProducts() async {
+  final response = await _supabase
+      .from('products')
+      .select();
+
+  return response;
 }
+
+}
+
