72880538ab17d6b92f2a8b5a3626c849f005809c
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-06 23:38:29 +0700

    bikin class cart service

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,3 +1,9 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:project_jasun/models/cart_item_model.dart';
 import 'package:project_jasun/models/product_model.dart';
+
+class CartService {
+  // Singleton Pattern
+  static final CartService _instance = CartService._internal();
+  factory CartService() => _instance;
+  CartService._internal();
