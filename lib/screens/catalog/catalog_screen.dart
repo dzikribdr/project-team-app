b4f2de9c4b1bb31783f42dc2e4cbf21e9b98f09e
berylrafi26 <berylrafi3@gmail.com>
2026-01-06 15:56:07 +0700

    menambahkan initialCategoryId

--- lib/screens/catalog/catalog_screen.dart
+++ lib/screens/catalog/catalog_screen.dart
@@ -4,3 +4,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:project_jasun/core/constants.dart';
 import 'package:project_jasun/models/product_model.dart';
 import 'package:project_jasun/screens/product/product_detail_screen.dart';
+
+class CatalogScreen extends StatefulWidget {
+  final int initialCategoryId;
+
+  const CatalogScreen({
+    super.key, 
+    this.initialCategoryId = 0 // 0 artinya Tampilkan Semua
+  });
