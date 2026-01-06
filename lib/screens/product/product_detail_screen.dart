822fb25f44d7f7fec2bcf29dbde9d47366d83b7f
berylrafi26 <berylrafi3@gmail.com>
2026-01-06 15:25:44 +0700

    menambahkah product

--- lib/screens/product/product_detail_screen.dart
+++ lib/screens/product/product_detail_screen.dart
@@ -3,3 +3,12 @@ import 'package:intl/intl.dart';
 import 'package:project_jasun/core/constants.dart';
 import 'package:project_jasun/models/product_model.dart';
 import 'package:project_jasun/services/cart_service.dart';
+
+class ProductDetailScreen extends StatefulWidget {
+  final Product product;
+
+  const ProductDetailScreen({super.key, required this.product});
+
+  @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
