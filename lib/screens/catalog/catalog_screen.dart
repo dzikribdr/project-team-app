72b6a400185092d64294b4bd863a215b8c537d64
berylrafi26 <berylrafi3@gmail.com>
2026-01-06 15:57:14 +0700

    menambahkan selectedCategoryId

--- lib/screens/catalog/catalog_screen.dart
+++ lib/screens/catalog/catalog_screen.dart
@@ -12,3 +12,17 @@ class CatalogScreen extends StatefulWidget {
     super.key, 
     this.initialCategoryId = 0 // 0 artinya Tampilkan Semua
   });
+
+  @override
+  State<CatalogScreen> createState() => _CatalogScreenState();
+}
+
+class _CatalogScreenState extends State<CatalogScreen> {
+  final SupabaseClient _supabase = Supabase.instance.client;
+  late int _selectedCategoryId;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedCategoryId = widget.initialCategoryId;
+  }
