f8702c2d613e8b6e4e2a0d04849588186970d0c4
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-06 22:31:47 +0700

    bikin class checkout screen

--- lib/screens/cart/checkout_screen.dart
+++ lib/screens/cart/checkout_screen.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:project_jasun/services/cart_service.dart';
 import 'package:project_jasun/core/constants.dart';
 import 'package:project_jasun/screens/main_screen.dart';
+
+class CheckoutScreen extends StatefulWidget {
+  const CheckoutScreen({super.key});
+
+  @override
+  State<CheckoutScreen> createState() => _CheckoutScreenState();
+}
