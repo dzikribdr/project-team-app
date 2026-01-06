e570ffa3b53b699101bb87877839f6cc3e7f2030
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-06 23:27:21 +0700

    masukkin import dan dua import yg wajib

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:project_jasun/core/constants.dart';
+// Dua import ini WAJIB ada dan DIPAKAI di bawah:
+import 'package:project_jasun/screens/auth/login_screen.dart';
+import 'package:project_jasun/screens/main_screen.dart';
+
