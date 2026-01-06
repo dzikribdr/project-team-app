3f51495ab2f18897b7c4c6431fe93a4eb5553e34
berylrafi26 <berylrafi3@gmail.com>
2026-01-06 13:53:56 +0700

    menambahkan class homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -6,3 +6,10 @@ import 'package:project_jasun/models/product_model.dart';
 import 'package:project_jasun/services/product_service.dart';
 import 'package:project_jasun/screens/catalog/catalog_screen.dart';
 import 'package:project_jasun/screens/product/product_detail_screen.dart';  
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
