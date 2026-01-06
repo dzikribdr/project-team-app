91e39101eb99a7297362e175c456911faa4c9700
vanstrong4 <alfeushendri@gmail.com>
2026-01-06 17:33:19 +0700

    buat retrun listView

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -175,3 +175,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ),
                   );
                 }
+
+                 return ListView.separated(
+                  padding: const EdgeInsets.all(16),
+                  itemCount: orders.length,
+                  separatorBuilder: (_, __) => const SizedBox(height: 12),
+                  itemBuilder: (context, index) {
+                    final order = orders[index];
+                    return Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: AppConstants.cardBgColor,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
