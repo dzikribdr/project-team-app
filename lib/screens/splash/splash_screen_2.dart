af83082d658fbd294fe8638a8474db1f8e5605cb
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-06 23:02:36 +0700

    buat class splashscreen2

--- lib/screens/splash/splash_screen_2.dart
+++ lib/screens/splash/splash_screen_2.dart
@@ -2,3 +2,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:project_jasun/core/constants.dart';
 import 'package:project_jasun/screens/auth/login_screen.dart';
+
+class SplashScreen2 extends StatefulWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  State<SplashScreen2> createState() => _SplashScreen2State();
+}
