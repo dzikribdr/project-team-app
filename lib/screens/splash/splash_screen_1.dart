975ab32cc18497b854857c984c4ee568ffb8a042
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-06 22:56:21 +0700

    bikin class splashscreen 1

--- lib/screens/splash/splash_screen_1.dart
+++ lib/screens/splash/splash_screen_1.dart
@@ -1,3 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart'; // Sesuaikan jika path beda
 import 'package:project_jasun/screens/splash/splash_screen_2.dart'; // Import Splash 2
+
+class SplashScreen1 extends StatefulWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  State<SplashScreen1> createState() => _SplashScreen1State();
+}
