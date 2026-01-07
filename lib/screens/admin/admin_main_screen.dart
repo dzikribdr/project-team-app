37f8ba955c859ad853c69aa310d25c75335b1ab3
Watanabeharuto5 <karismaavibra@gmail.com>
2026-01-07 23:58:15 +0700

    class admin-orders

--- lib/screens/admin/admin_main_screen.dart
+++ lib/screens/admin/admin_main_screen.dart
@@ -6,3 +6,18 @@ import 'package:project_jasun/screens/admin/admin_users_screen.dart';
 import 'package:project_jasun/screens/auth/login_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+class AdminMainScreen extends StatefulWidget {
+  const AdminMainScreen({super.key});
+
+  @override
+  State<AdminMainScreen> createState() => _AdminMainScreenState();
+}
+
+class _AdminMainScreenState extends State<AdminMainScreen> {
+  int _selectedIndex = 0;
+  
+  final List<Widget> _pages = [
+    const AdminProductsScreen(),
+    const AdminOrdersScreen(),
+    const AdminUsersScreen(),
+  ];
