9048b2ca6c708199ee3e8038ec040687b6b0630e
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-09 00:38:07 +0700

    masukkin import dan bikin splashscreen6

---
+++ lib/screens/splash/splash_screen_6.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:project_jasun/core/constants.dart';
+import 'package:project_jasun/screens/auth/login_screen.dart';
+
+class SplashScreen6 extends StatelessWidget {
+  const SplashScreen6({super.key});
