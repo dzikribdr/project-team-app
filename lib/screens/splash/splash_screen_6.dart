e3cfb826075cbb699e77624118a70d6252b5bee6
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-09 00:38:45 +0700

    bikin return scaffold dan container

--- lib/screens/splash/splash_screen_6.dart
+++ lib/screens/splash/splash_screen_6.dart
@@ -4,3 +4,18 @@ import 'package:project_jasun/screens/auth/login_screen.dart';
 
 class SplashScreen6 extends StatelessWidget {
   const SplashScreen6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              AppConstants.primaryColor,
+              Color(0xFF0F2027),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
