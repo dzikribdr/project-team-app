ab3e6f9829e66c58dc6bb8c3c9e101cc11c1b5ff
Watanabeharuto5 <karismaavibra@gmail.com>
2026-01-08 09:10:34 +0700

    class admin users

--- lib/screens/admin/admin_users_screen.dart
+++ lib/screens/admin/admin_users_screen.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:project_jasun/core/constants.dart';
+
+class AdminUsersScreen extends StatelessWidget {
+  const AdminUsersScreen({super.key});
