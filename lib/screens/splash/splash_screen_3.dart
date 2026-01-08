62b194412c77a83f779677d56cc979a4beaceabb
Watanabeharuto5 <karismaavibra@gmail.com>
2026-01-08 09:43:04 +0700

    membuat image dalam container

--- lib/screens/splash/splash_screen_3.dart
+++ lib/screens/splash/splash_screen_3.dart
@@ -6,4 +6,25 @@ import 'package:project_jasun/screens/splash/splash_screen_3.dart';
 
 class SplashScreen3 extends StatelessWidget {
   const SplashScreen3({super.key});
-  
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child: Column(
+          //1. buat image dalam container
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            SizedBox(height:100),
+            Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              color:const Color.fromARGB(255, 250, 184, 179),
+              shape:BoxShape.circle,
+              image: DecorationImage(
+                  image: AssetImage("../assets/images/chibi.png"),
+              fit : BoxFit.cover
+                  ),
+            ),
+           ),
