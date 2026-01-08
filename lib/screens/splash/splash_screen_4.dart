d409def0c9510be81b57b53c73f003860865ee84
vanstrong4 <alfeushendri@gmail.com>
2026-01-08 10:17:53 +0700

    bikin class mysplashscreen2

--- lib/screens/splash/splash_screen_4.dart
+++ lib/screens/splash/splash_screen_4.dart
@@ -1,3 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:kb1179_1123150186_uts/screen/splash_screen_3.dart'; // Import aslimu
+
+class MySplashScreen2 extends StatelessWidget {
+  const MySplashScreen2({super.key});
