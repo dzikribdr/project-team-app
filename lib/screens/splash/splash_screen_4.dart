243f95d6ae15fd816b7109ef9fb6509bf5a37e24
vanstrong4 <alfeushendri@gmail.com>
2026-01-08 10:18:29 +0700

    bikin return scaffold

--- lib/screens/splash/splash_screen_4.dart
+++ lib/screens/splash/splash_screen_4.dart
@@ -4,3 +4,23 @@ import 'package:kb1179_1123150186_uts/screen/splash_screen_3.dart'; // Import as
 
 class MySplashScreen2 extends StatelessWidget {
   const MySplashScreen2({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          // Background Dekoratif (Glow di pojok)
+          Positioned(
+            top: -50,
+            right: -50,
+            child: Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber.withOpacity(0.15),
+              ),
+            ),
+          ),
