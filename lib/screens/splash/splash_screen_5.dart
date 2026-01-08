1a668d4e75bc0d2023abf0c407810a4a5dac5cae
vanstrong4 <alfeushendrievan@gmail.com>
2026-01-09 00:32:36 +0700

    bikin class splash screen 5 state

--- lib/screens/splash/splash_screen_5.dart
+++ lib/screens/splash/splash_screen_5.dart
@@ -4,3 +4,19 @@ import 'splash_screen_6.dart';
 
 class SplashScreen5 extends StatefulWidget {
   const SplashScreen5({super.key});
+
+  @override
+  State<SplashScreen5> createState() => _SplashScreen5State();
+}
+
+class _SplashScreen5State extends State<SplashScreen5> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const SplashScreen6()),
+      );
+    });
+  }
